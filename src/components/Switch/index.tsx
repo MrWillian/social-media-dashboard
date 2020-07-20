f29c9f2723c95f99a94ac73529e2cd5f811abff7
MrWilllian <willian.dev10@gmail.com>
2020-07-20 10:44:20 -0300

    refactor: add Switch component index file

---
+++ src/components/Switch/index.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import { Container, Switcher } from './styles';
+
+interface Props {
+  onChange(): void;
+  isChecked?: boolean;
+}
+
+const Switch: React.FC<Props> = ({ 
+  onChange, isChecked
+}) => {
+  const [checked, setChecked] = useState(isChecked);
+
+  const onClick = () => {
+    onChange();
+    setChecked(!checked);
+  }
+
+  return (
+    <Container
+      className={checked ? 'checked' : ''}>
+      
+      <Switcher
+        onClick={onClick} />
+
+    </Container>
+  );
+}
+
+export default Switch;
