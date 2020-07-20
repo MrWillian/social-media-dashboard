b348ccac1a48798e041c7b141efbfd96c85284d5
MrWilllian <willian.dev10@gmail.com>
2020-07-20 10:47:12 -0300

    feat: include theme provider with all theme change settings - v1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import './App.css';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
+import usePersistedState from './utils/usePersistedState';
 
-function App() {
+import dark from './styles/themes/dark';
+import light from './styles/themes/light';
+
+import GlobalStyles from './styles/GlobalStyles';
+import Dashboard from './pages/Dashboard';
+
+const App = () => {
+  const [theme, setTheme] = 
+    usePersistedState<DefaultTheme>('@social-media/theme', dark);
+  
+  const toggleTheme = () => 
+    setTheme(theme.title === 'light' ? dark : light);
+  
   return (
-    <div />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Dashboard toggleTheme={toggleTheme} />
+    </ThemeProvider>
   );
 }
 
