7c550c53d0c961937c79c212038df6898774ea48
MrWilllian <willian.dev10@gmail.com>
2020-07-20 10:44:56 -0300

    style: add Header component style file

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+
+  padding: 20px;
+
+  background-color: ${props => props.theme.colors.background_header};
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+  font-weight: bold;
+  font-size: var(--font-size);
+
+  h2 {
+    color: ${props => props.theme.colors.text};
+    margin-top: 20px;
+  }
+
+  span {
+    color: ${props => props.theme.colors.text_card};
+    margin-top: 5px;
+  }
+`;
+
+export const Divisor = styled.hr`
+  margin-top: 1.5em; 
+  margin-bottom: 0.5em; 
+  border: 1px solid rgba(255,255,255,.05);
+  min-width: 100%;
+`;
+
+export const ThemeSwitcherContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  min-width: 100%;
+
+  .switch {
+
+  }
+`;
+
