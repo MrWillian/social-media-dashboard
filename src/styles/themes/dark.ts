738ca3ff345ff4ef9820ca2f5a0d421eb0467840
MrWilllian <willian.dev10@gmail.com>
2020-07-20 10:05:06 -0300

    style: add dark theme file

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,16 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    background: 'hsl(230, 17%, 14%)',
+    background_header: 'hsl(232, 19%, 15%)',
+
+    background_card: 'hsl(232, 19%, 15%)',
+    text_card: 'hsl(228, 34%, 66%)',
+
+    toggle: '#3eda82',
+    off_toggle: 'hsl(230, 22%, 74%)',
+
+    text: 'hsl(0, 0%, 100%)'
+  },
+}
