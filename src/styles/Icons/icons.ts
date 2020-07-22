27b54a435e082c13349bd1b3e3f5555b286a6e73
MrWilllian <willian.dev10@gmail.com>
2020-07-22 10:39:44 -0300

    style: add icons export file

---
+++ src/styles/Icons/icons.ts
@@ -0,0 +1,6 @@
+export { DownIcon } from './DownIcon';
+export { FacebookIcon } from './FacebookIcon';
+export { InstagramIcon } from './InstagramIcon';
+export { TwitterIcon } from './TwitterIcon';
+export { UpIcon } from './UpIcon';
+export { YoutubeIcon } from './YoutubeIcon';
