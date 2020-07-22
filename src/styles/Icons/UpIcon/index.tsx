7629cbf3b27b34fced813193979c3ee5ebdf1acc
MrWilllian <willian.dev10@gmail.com>
2020-07-22 10:39:21 -0300

    style: add icons

---
+++ src/styles/Icons/UpIcon/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Icon } from '../../../interfaces/iSocialMedia';
+
+export const UpIcon: React.FC<Icon> = () => {
+  return (
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      width="8" 
+      height="4"
+    >
+      <path 
+        fill="#1EB589" 
+        fillRule="evenodd" 
+        d="M0 4l4-4 4 4z" />
+    
+    </svg>
+  );
+}
