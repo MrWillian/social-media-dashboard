ed892cf59326ed3c5b2313138d4a92e99f20c885
MrWilllian <willian.dev10@gmail.com>
2020-07-22 10:44:13 -0300

    refactor: remove Icon type from model

--- src/models/SocialMedia.ts
+++ src/models/SocialMedia.ts
@@ -1,21 +1,18 @@
-import { iSocialMedia, Icon } from '../interfaces/iSocialMedia';
+import { iSocialMedia } from '../interfaces/iSocialMedia';
 
 export class SocialMedia implements iSocialMedia {
   public title: string;
   public color: string;
-  public icon: Icon;
 
-  constructor(title: string, color: string, icon: Icon) {
+  constructor(title: string, color: string) {
     this.title = title;
     this.color = color;
-    this.icon = icon;
   }
 
   public operation(): object {
     return {
       "title": this.title, 
-      "color": this.color, 
-      "icon": this.icon
+      "color": this.color,
     };
   }
 }
