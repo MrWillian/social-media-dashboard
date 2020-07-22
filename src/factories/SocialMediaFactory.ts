79ed6870c093b64c339f635e12d15e2b287d4718
MrWilllian <willian.dev10@gmail.com>
2020-07-22 10:44:22 -0300

    refactor: remove Icon type from factory

--- src/factories/SocialMediaFactory.ts
+++ src/factories/SocialMediaFactory.ts
@@ -1,27 +1,20 @@
 import { SocialMedia } from '../models/SocialMedia';
 import { SOCIAL_MEDIA } from '../enums/SocialMediaEnum';
 
-import {
-  FacebookIcon,
-  InstagramIcon,
-  TwitterIcon,
-  YoutubeIcon,
-} from '../styles/Icons/icons';
-
 export default function createSocialMedia(socialMedia: string): SocialMedia | undefined {
   try {
     switch(socialMedia) {
       case SOCIAL_MEDIA.FACEBOOK:
-        return new SocialMedia('Facebook', 'hsl(208, 92%, 53%)', FacebookIcon);
+        return new SocialMedia('Facebook', 'hsl(208, 92%, 53%)');
       
       case SOCIAL_MEDIA.INSTAGRAM:
-        return new SocialMedia('Instagram', 'linear-gradient(hsl(37, 97%, 70%), hsl(329, 70%, 58%))', InstagramIcon);
+        return new SocialMedia('Instagram', 'linear-gradient(to right, hsl(37, 97%, 70%), hsl(329, 70%, 58%))');
       
       case SOCIAL_MEDIA.TWITTER:
-        return new SocialMedia('Twitter', 'hsl(203, 89%, 53%)', TwitterIcon);
+        return new SocialMedia('Twitter', 'hsl(203, 89%, 53%)');
       
       case SOCIAL_MEDIA.YOUTUBE:
-        return new SocialMedia('Youtube', 'hsl(348, 97%, 39%)', YoutubeIcon);
+        return new SocialMedia('Youtube', 'hsl(348, 97%, 39%)');
     }
   } catch (error) {
     return error;
