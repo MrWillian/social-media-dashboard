08e7ebeae317469c64010b248ae1c62dca91d52d
MrWilllian <willian.dev10@gmail.com>
2020-07-23 14:56:45 -0300

    style: new changes to responsivity

--- src/components/OverviewContainer/styles.ts
+++ src/components/OverviewContainer/styles.ts
@@ -4,9 +4,25 @@ export const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   margin-bottom: 30px;
+
+  @media(min-width: 500px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media(min-width: 800px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
 `;
 
 export const Title = styled.h2`
   margin: 20px;
   color: ${props => props.theme.colors.text};
+
+  @media(min-width: 500px) {
+    grid-column: span 2;
+  }
+
+  @media(min-width: 800px) {
+    grid-column: span 4;
+  }
 `;
